8bb3b9f8ca604cdbd2cd54abd92298a9fab8a78a
Bach Xuan Ngo <bach@lnxsrv15.seas.ucla.edu>
2023-05-24 14:10:47 -0700

    changed mrand's seed to change every time we call randall

--- mrand.c
+++ mrand.c
@@ -1,15 +1,23 @@
 #include "mrand.h"
 #include <stdlib.h>
+#include <time.h>
 
 void mrand48_init(char* filepath) {}
 
 unsigned long long mrand48_rng(void) {
-  struct drand48_data buf = {0};
-  srand48_r(69, &buf);
-  long int a, b;
-  mrand48_r(&buf, &a);
-  mrand48_r(&buf, &b);
-  return (((unsigned long long) a) << 32) | ((unsigned long long) b & 0x00000000FFFFFFFF);
+  struct drand48_data randbuffer;
+  long int seed = time(NULL);
+  srand48_r(seed, &randbuffer);
+  //  fprintf(stderr, "%li\n", seed);
+  
+  // mrand creates a 32 bit, but we need a 64 bit 
+  long int upper, lower;
+  mrand48_r(&randbuffer, &upper);
+  mrand48_r(&randbuffer, &lower);
+
+  // fprintf(stderr, "rands: %x, %x\n", (unsigned int)upper, (unsigned int)lower);
+  
+  return (((unsigned long long) upper) << 32) | ((unsigned long long) lower);
 }
 
 void mrand48_fini(void) {}
